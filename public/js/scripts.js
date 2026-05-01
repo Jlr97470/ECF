d97066d6071ac64cabd0d323f536d1e0c2408a55
jlr97442 <royerjeanlaurent@outlook.fr>
2026-05-01 23:16:13 +0400

    Version 20260401-2

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,3 +5,127 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+const OkOnly=1;
+const OkCancel=2;
+const YesNo=4;
+const YesNoCancel=8;
+
+function InitDialog(style,titre,message,func)
+{
+	const divdialog = document.getElementById('divdialog');				
+	let dialoghtml;
+	dialoghtml='<dialog id="diadialog">';
+	dialoghtml=dialoghtml+'<h2>'+titre+'</h2>';
+	dialoghtml=dialoghtml+'<p>'+message+'</p>';
+	dialoghtml=dialoghtml+'<div class="boutons-actions">';
+
+	switch (style)
+		{
+			case OkOnly:
+				{
+					dialoghtml=dialoghtml+'<button id="btnOk" type="button">Ok</button>';
+					break;
+				}
+			case OkCancel:
+				{
+					dialoghtml=dialoghtml+'<button id="btnOk" type="button">Ok</button>';
+					dialoghtml=dialoghtml+'<button id="btnCancel" type="button">Cancel</button>';
+					break;
+				}
+			case YesNo:
+				{
+					dialoghtml=dialoghtml+'<button id="btnYes" type="button">Oui</button>';
+					dialoghtml=dialoghtml+'<button id="btnNo" type="button">Non</button>';
+					break;
+				}
+			case YesNoCancel:
+				{
+					dialoghtml=dialoghtml+'<button id="btnYes" type="button">Oui</button>';
+					dialoghtml=dialoghtml+'<button id="btnNo" type="button">Non</button>';
+					dialoghtml=dialoghtml+'<button id="btnCancel" type="button">Cancel</button>';								
+					break;
+				}
+		}
+
+	dialoghtml=dialoghtml+'</div>';
+	dialoghtml=dialoghtml+'</dialog>';
+
+	divdialog.innerHTML=dialoghtml;
+
+	const dialogue = document.getElementById('diadialog');				
+
+	switch (style)
+		{
+			case OkOnly:
+				{
+					const btnOk = document.getElementById('btnOk');
+
+					// Annuler et fermer
+					btnOk.addEventListener('click', () => {
+					  dialogue.close('Ok');
+					});
+					break;
+				}
+			case OkCancel:
+				{
+					const btnOk = document.getElementById('btnOk');
+
+					// Annuler et fermer
+					btnOk.addEventListener('click', () => {
+					  dialogue.close('Ok');
+					});
+					const btnCancel = document.getElementById('btnCancel');
+
+					// Annuler et fermer
+					btnCancel.addEventListener('click', () => {
+					  dialogue.close('Cancel');
+					});
+					break;
+				}
+			case YesNo:
+				{
+					const btnYes = document.getElementById('btnYes');
+
+					// Annuler et fermer
+					btnYes.addEventListener('click', () => {
+					  dialogue.close('Yes');
+					});
+					const btnNo = document.getElementById('btnNo');
+
+					// Annuler et fermer
+					btnNo.addEventListener('click', () => {
+					  dialogue.close('No');
+					});
+					break;
+				}
+			case YesNoCancel:
+				{
+					const btnYes = document.getElementById('btnYes');
+
+					// Annuler et fermer
+					btnYes.addEventListener('click', () => {
+					  dialogue.close('Yes');
+					});
+					const btnNo = document.getElementById('btnNo');
+
+					// Annuler et fermer
+					btnNo.addEventListener('click', () => {
+					  dialogue.close('No');
+					});
+					break;
+					const btnCancel = document.getElementById('btnCancel');
+
+					// Annuler et fermer
+					btnCancel.addEventListener('click', () => {
+					  dialogue.close('Cancel');
+					});
+					break;
+				}
+		}
+
+	dialogue.addEventListener('close', func);				
+
+}
+
+//-->// JavaScript Document
